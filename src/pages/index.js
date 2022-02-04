92fea0d13a07ea05a1f0c7a810c190c047ed309b
Khlustov <khlustov.alex@gmail.com>
2022-02-04 17:56:13 +0300

    add: contentful, links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,33 @@
-import React from "react"
+import '../styles/global.less';
+import '../styles/homePage.less';
 
-const Home = () => {
-  return <div>Hello world!</div>
+import React from "react"
+import {graphql, Link} from "gatsby";
+
+const Home = ({ data }) => {
+    const links = data.allContentfulTemplate.nodes
+
+  return (
+      <div className="home-page">
+        <div className="links-container">
+            {links.map(link => (
+                <Link to={`/${link.slug}`} className="link">{link.title}</Link>
+            ))}
+        </div>
+      </div>
+  )
 }
 
 export default Home;
+
+
+export const query = graphql`
+query HomePageQuery {
+  allContentfulTemplate {
+    nodes {
+      title
+      slug
+    }
+  }
+}
+`
